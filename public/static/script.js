40fd6076e767f2bd1502c00cefc9c67be286f66b
Brandon Chupp <15858979+brandonchupp@users.noreply.github.com>
2022-12-29 07:58:20 -0800

    Share link for category lists
    Add a button which opens the messages app (assuming mobile device)
    and provides a link with the current random word lists.
    
    This allows a user to share their list with others. The imagined use-case
    is several users all using their own devices to view the word lists.

--- public/static/script.js
+++ public/static/script.js
@@ -1,10 +1,13 @@
-let NUMBER_OF_CATEGORIES = 12;
-let NUMBER_OF_LISTS = 3;
+const NUMBER_OF_CATEGORIES = 12;
+const NUMBER_OF_LISTS = 3;
+const STATE_DELIMITER = "z";
 
-function getWordList() {
+let categoriesState = [];
+
+function getWordList(state) {
     fetch('/static/categories.txt')
         .then(data => data.text())
-        .then(processWordList)
+        .then(text => processWordList(text, state))
         .catch(error => new Error(error));
 }
 
@@ -12,7 +15,7 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 
-function processWordList(wordList) {
+function processWordList(wordList, state) {
     let categories = wordList.split("\n");
 
     renderedLists = '';
@@ -20,19 +23,51 @@ function processWordList(wordList) {
     for (let list = 1; list < NUMBER_OF_LISTS + 1; list++) {
         renderedLists += `<details><summary>List ${list}</summary><ol>`;
         for (let category = 0; category < NUMBER_OF_CATEGORIES; category++) {
-            let categoryIdx = getRandomInt(categories.length);
+            let categoryIdx;
+            if (state) {
+                // Grab the first available state index
+                categoryIdx = state.splice(0, 1);
+            } else {
+                categoryIdx = getRandomInt(categories.length);
+            }
             let categoryText = categories.splice(categoryIdx, 1);
+
+            // Store the indexes so they can be used to rebuild the word lists.
+            categoriesState.push(categoryIdx);
 
             renderedLists += `<li>${categoryText}</li>`;
         }
         renderedLists += `</ol></details>`;
     }
+
+    renderedLists += getSharableLink();
+
     document.querySelector(".content").innerHTML = renderedLists;
+
+}
+
+function getSharableLink() {
+    let state = categoriesState.join(STATE_DELIMITER);
+    let URL = `${window.location.href}?state=${encodeURIComponent(state)}`;
+    return `
+        <a role="button" href="sms:?&body=View the category lists: ${URL}">
+            Share
+        </a>
+    `;
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+    // Parse query parameters
+    const params = (new URL(location)).searchParams;
+    if (params.get("state")) {
+        let state = params.get("state").split(STATE_DELIMITER);
+        getWordList(state);
+    }
+
     document.querySelector("#random-list").addEventListener(
         "click",
-        getWordList
+        function () {
+            getWordList();
+        }
     );
 }, false);
